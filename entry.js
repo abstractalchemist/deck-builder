388e7fd0e51b2d333345de8bc46e9bf198a5940e
Jason Hirata <jhirata@integrity-apps.com>
2017-10-06 21:08:04 -1000

    modified card store to call mongodb directly

---
+++ entry.js
@@ -0,0 +1,49 @@
+const http = require('http');
+
+const db = process.argv[2];
+const host = process.argv[3];
+
+const mapper = function(doc) {
+    if(doc) {
+	emit(null,doc)
+    }
+}
+
+const lister = function(head,req) {
+    send("[");
+    row = getRow();
+    send(JSON.stringify(row.value));
+    
+    while(row = getRow()) {
+	send("," + JSON.stringify(row.value));
+    }
+    send("]");
+}
+ 
+let uuid = http.request({ method: "GET", host : host, port: "5984", path : "/_uuids"}, res => {
+    let buffer = "";
+    res.on('data', data => buffer += "" + data)
+    res.on('end', _ => {
+	let ddoc = http.request({ method: "PUT", host: host, port: "5984", path: "/" + db + "/_design/view"}, res => {
+	    buffer = "";
+	    res.on('data', data => buffer += "" + data);
+	    res.on('end', _ => {
+		console.log(buffer)
+	    })
+	})
+
+	ddoc.write(JSON.stringify({
+	    views : {
+		all : {
+		    map : mapper.toString()
+		}
+	    },
+	    lists : {
+		all : lister.toString()
+	    }
+	}));
+	ddoc.end();
+    })
+})
+uuid.write('');
+uuid.end();
--- src/card_store.js
+++ src/card_store.js
@@ -2,11 +2,12 @@ import Http from './utils';
 import Rx from 'rx';
 
 export default (function() {
-    let testing = [{id:"VS/W50-033",level:3,image:"https://images.littleakiba.com/tcg/card37121-large.jpg",abilities:["【A】 When this card is placed on Stage from Hand, you may place the top card of your Clock into Waiting Room.", "【A】 This ability can only be activated up to 1 time each turn. When you used 【S】, during this turn, this card gets +X Power. X is equals to the number of your other 《格闘》 Characters x500.", "【A】【CXCOMBO】 When this card attacks, if 「アクセルスマッシュ・インフィニティ」 is in the Climax slot, until the end of your opponent's next turn, this card gets +1500 Power, gains the following ability.『【A】 When the Character facing this card attacks, you may deal 1 Damage to your opponent.』(Damage can be cancelled)"]},
-		   {id:"VS/W50-043",level:0,image:"https://images.littleakiba.com/tcg/card37047-medium.jpg"},
-		   {id:"VS/W50-003",level:3,image:"https://images.littleakiba.com/tcg/card37009-medium.jpg"},
-		   {id:"VS/W50-069",level:3,image:"https://images.littleakiba.com/tcg/card37073-medium.jpg"},
-		   {id:"VS/W50-010",level:3,image:"https://images.littleakiba.com/tcg/card37016-medium.jpg"}]
+    let testing = [{id:"033",level:3,image:"https://images.littleakiba.com/tcg/card37121-large.jpg",abilities:["【A】 When this card is placed on Stage from Hand, you may place the top card of your Clock into Waiting Room.", "【A】 This ability can only be activated up to 1 time each turn. When you used 【S】, during this turn, this card gets +X Power. X is equals to the number of your other 《格闘》 Characters x500.", "【A】【CXCOMBO】 When this card attacks, if 「アクセルスマッシュ・インフィニティ」 is in the Climax slot, until the end of your opponent's next turn, this card gets +1500 Power, gains the following ability.『【A】 When the Character facing this card attacks, you may deal 1 Damage to your opponent.』(Damage can be cancelled)"]},
+		   {id:"043",level:0,image:"https://images.littleakiba.com/tcg/card37047-medium.jpg"},
+		   {id:"003",level:3,image:"https://images.littleakiba.com/tcg/card37009-medium.jpg"},
+		   {id:"069",level:3,image:"https://images.littleakiba.com/tcg/card37073-medium.jpg"},
+		   {id:"010",level:3,image:"https://images.littleakiba.com/tcg/card37016-medium.jpg"}]
+    let selecteddb;
     return {
 
 	getownership(card_id) {
@@ -18,19 +19,23 @@ export default (function() {
 
 	// get a card based on id
 	getcard(card_id) {
-	    return Rx.Observable.fromArray(testing.filter(({id}) => card_id === id));
+	    //return Rx.Observable.fromArray(testing.filter(({id}) => card_id === id));
+	    return Rx.Observable.fromPromise(Http({method:"GET",url:"/api/" + selecteddb + "/" + card_id})).map(JSON.parse).map(obj => Object.assign({},obj, {id:obj._id}));
 	},
 
 	// get all cards from a card set id
 	getcardsfromset(id) {
-	    if(id === 'VS') {
-		return Rx.Observable.fromArray(testing);
-	    }
+//	    if(id === 'VS') {
+//		return Rx.Observable.fromArray(testing);
+	    //	    }
+	    selecteddb = id;
+	    return Rx.Observable.fromPromise(Http({method:"GET",url:"/api/" + id + "/_design/view/_list/all/all"})).map(JSON.parse).selectMany(Rx.Observable.fromArray).map(obj => Object.assign({},obj,{id:obj._id}));
 	},
 
 	// get all known card sets
 	getcardsets() {
-	    return Rx.Observable.fromArray([{id:"VS",label:"Vivid Strke"}]).toArray();
+	    //	    return Rx.Observable.fromArray([{id:"VS",label:"Vivid Strke"}]).toArray();
+	    return Rx.Observable.fromPromise(Http({method:"GET",url:"/api/cardsets/sets"})).map(JSON.parse).pluck('sets');
 	}
     };
 })()
