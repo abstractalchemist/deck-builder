bb2c8dc2719c4e51e1cc28777f1c132b82e11884
Jason Hirata <jhirata@integrity-apps.com>
2017-11-15 08:44:38 -1000

    added webstorage option for offline usage

--- src/card_store.js
+++ src/card_store.js
@@ -1,5 +1,5 @@
 import Http from 'utils';
-import Rx from 'rxjs/Rx';
+import Rx from 'rxjs';
 
 export default (function() {
     let testing = [{id:"033",level:3,image:"https://images.littleakiba.com/tcg/card37121-large.jpg",abilities:["【A】 When this card is placed on Stage from Hand, you may place the top card of your Clock into Waiting Room.", "【A】 This ability can only be activated up to 1 time each turn. When you used 【S】, during this turn, this card gets +X Power. X is equals to the number of your other 《格闘》 Characters x500.", "【A】【CXCOMBO】 When this card attacks, if 「アクセルスマッシュ・インフィニティ」 is in the Climax slot, until the end of your opponent's next turn, this card gets +1500 Power, gains the following ability.『【A】 When the Character facing this card attacks, you may deal 1 Damage to your opponent.』(Damage can be cancelled)"]},
@@ -9,8 +9,22 @@ export default (function() {
 		   {id:"010",level:3,image:"https://images.littleakiba.com/tcg/card37016-medium.jpg"}]
     let selecteddb;
 
-    let mapper = card_id => {
-	return Rx.Observable.fromPromise(Http({method:"GET",url:"/api/cardmapping/mapping"}))
+
+    // this is a reverse mapper
+    const mapper = card_id => {
+	return Rx.Observable.from(Http({method:"GET",url:"/api/cardmapping/mapping"}))
+	    .catch(err => {
+		console.log(err)
+		if(window.localStorage)
+		    return Rx.Observable.from(window.localStorage.getItem('mapping'))
+		throw err;
+	    })
+
+	    .do(data => {
+		if(window.localStorage) {
+		    window.localStorage.setItem('mapping', data)
+		}
+	    })
 	    .map(JSON.parse)
 	    .map(({mapping}) => {
 		//		    let db = mapping.find(({prefix}) => card_id.startsWith(prefix));
@@ -29,26 +43,39 @@ export default (function() {
 
     }
     
-    let cardmapper = card_id => {
+    const cardmapper = card_id => {
 	return mapper(card_id)
-	    .mergeMap(db =>Rx.Observable.fromPromise(Http({method:"GET",url:"/api/" + db.db + "/" + card_id})).map(JSON.parse).map(obj => Object.assign({},obj, {id:obj._id})));
+	    .mergeMap(db => Http({method:"GET",url:"/api/" + db.db + "/" + card_id}))
+	    
+	    .catch(err => {
+		if(window.localStorage) {
+		    return Rx.Observable.from(window.localStorage.getItem(card_id))
+		}
+		throw err
+	    })
+	    .do(data => {
+		if(window.localStorage)
+		    window.localStorage.setItem(card_id, data)
+	    })
+	    .map(JSON.parse)
+	    .map(obj => Object.assign({},obj, {id:obj._id}));
 		
     }
     
     return {
 
 	removefromcollection(card_id) {
-	    return Rx.Observable.fromPromise(Http({method:"GET",url:"/api/library/" + card_id}))
+	    return Rx.Observable.from(Http({method:"GET",url:"/api/library/" + card_id}))
 		.catch(_ => {
 		    return Rx.Observable.of();
 		})
 		.map(JSON.parse)
 		.mergeMap(({count,_rev}) => {
 		    if(count === 1)
-			return Rx.Observable.fromPromise(Http({method:"DELETE",url:"/api/library/"+card_id+"?rev=" + _rev}))
+			return Http({method:"DELETE",url:"/api/library/"+card_id+"?rev=" + _rev})
 		    else
 			    
-			return Rx.Observable.fromPromise(Http({method:"PUT",url:"/api/library/"+card_id}, JSON.stringify({count:count-1,_rev})))
+			return Http({method:"PUT",url:"/api/library/"+card_id}, JSON.stringify({count:count-1,_rev}))
 		})
 			
 	},
@@ -66,20 +93,18 @@ export default (function() {
 		
 	},
 	addtocollection(card_id) {
-	    return Rx.Observable.fromPromise(Http({method:"GET",url:"/api/library/" + card_id}))
+	    return Rx.Observable.from(Http({method:"GET",url:"/api/library/" + card_id}))
 		.map(JSON.parse)
-		.mergeMap(({count,_rev}) => {
-		    return Rx.Observable.fromPromise(Http({method:"PUT",url:"/api/library/" + card_id}, JSON.stringify({count:count+1,_rev})))
-		})
-		.catch(_ => Rx.Observable.fromPromise(Http({method:"PUT",url:"/api/library/" + card_id}, JSON.stringify({count:1}))));
+		.mergeMap(({count,_rev}) => Http({method:"PUT",url:"/api/library/" + card_id}, JSON.stringify({count:count+1,_rev})))
+		.catch(_ => Rx.Observable.from(Http({method:"PUT",url:"/api/library/" + card_id}, JSON.stringify({count:1}))));
 	},
 	
 	getownership(card_id) {
 	    if(!card_id)
 		throw "Card Id not provided"
-	    return Rx.Observable.fromPromise(Http({method:"GET",url:"/api/library/" + card_id.toLowerCase().replace(/\/|-/g,"_")}))
+	    return Rx.Observable.from(Http({method:"GET",url:"/api/library/" + card_id.toLowerCase().replace(/\/|-/g,"_")}))
 		.map(JSON.parse)
-		.catch(_ => Rx.Observable.fromPromise(Http({method:"GET", url:"/api-dyn/price/" + card_id}))
+		.catch(_ => Rx.Observable.from(Http({method:"GET", url:"/api-dyn/price/" + card_id}))
 		       .catch(err => Rx.Observable.of("No Price found"))
 	    	       .map(price => {
 	    		   return { count: 0, price }
@@ -98,22 +123,41 @@ export default (function() {
 
 	// get all cards from a card set id
 	getcardsfromset(id) {
-//	    if(id === 'VS') {
-//		return Rx.Observable.from(testing);
-	    //	    }
-	    //	    selecteddb = id;
-	    return Rx.Observable.fromPromise(Http({method:"GET",url:"/api/" + id + "/_design/view/_list/all/all"}))
-		.map(JSON.parse)
-		.mergeMap(data => {
-		    return Rx.Observable.from(data)
+	    return Rx.Observable.from(Http({method:"GET",url:"/api/" + id + "/_design/view/_list/all/all"}))
+
+		.catch(err => {
+		    if(window.localStorage) {
+			return Rx.Observable.from(window.localStorage.getItem(id))
+		    }
+		    throw err
 		})
+		.do(data => {
+		    if(window.localStorage)
+			window.localStorage.setItem(id,data)
+		})
+	    	.map(JSON.parse)
+		.mergeMap(data => Rx.Observable.from(data))
 		.map(obj => Object.assign({},obj,{id:obj._id}));
 	},
 
 	// get all known card sets
 	getcardsets() {
-	    //	    return Rx.Observable.from([{id:"VS",label:"Vivid Strke"}]).toArray();
-	    return Rx.Observable.fromPromise(Http({method:"GET",url:"/api/cardsets/sets"})).map(JSON.parse).pluck('sets');
+
+	    return Rx.Observable.from(Http({method:"GET",url:"/api/cardsets/sets"}))
+
+		.catch(err => {
+		    if(window.localStorage) {
+			return Rx.Observable.from(window.localStorage.getItem('sets'))
+		    }
+		    else
+			throw err;
+		})
+		.do(data => {
+		    if(window.localStorage)
+			window.localStorage.setItem('sets', data)
+		})
+	    	.map(JSON.parse)
+		.pluck('sets');
 	}
     };
 })()
