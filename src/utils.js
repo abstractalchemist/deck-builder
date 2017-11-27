f138860bc10bd91c3e2d38fff7039e8ef117369f
Jason Hirata <jhirata@integrity-apps.com>
2017-11-26 17:30:28 -1000

    fixing mobile code

--- src/utils.js
+++ src/utils.js
@@ -4,12 +4,12 @@ import { Card } from 'ui-utils';
 
 function generateCard(addHandler, removeHandler) {
     return function(card) {
-	return (<div className="mdl-cell mdl-cell--3-col" style={{ maxWidth:"300px" }}>
-		<Card {...card} addhandler={addHandler} removehandler={removeHandler}>
+	return (<div className="mdl-cell mdl-cell--3-col-desktop mdl-cell--2-col-phone mdl-cell--2-col-tablet card-set-cell">
+		<Card {...card} addhandler={{handler:addHandler,tooltip:"Add One To Deck"}} removehandler={{handler:removeHandler,tooltip:"Remove One From Deck"}}>
 		    {(_ => {
 			if(card.abilities) {
 			    let i = 0;
- 			    return card.abilities.map( text => <p style={{fontSize:"10px",lineHeight:"12px"}} key={i++}>{text}</p>)
+ 			    return card.abilities.map( text => <p key={i++}>{text}</p>)
 			}
 		    })()}
 		
