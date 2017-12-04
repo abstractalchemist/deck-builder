1f0041549ab7d8837a319e4c887f0e9aa71c9dbf
Jason Hirata <jhirata@integrity-apps.com>
2017-12-04 12:01:45 -1000

    fixing sort

--- src/worker.js
+++ src/worker.js
@@ -2,7 +2,7 @@ export default function(self) {
     console.log("instantiating worker")
     self.addEventListener('message', ({data}) => {
 	console.log(`received message`)
-	data.sort()
+	data.sort(({id:id0}, {id:id1}) => id0.localeCompare(id1))
 	self.postMessage(data)
     })
 }
